67c86802c31acfb219ccbd88ab7c53eefa90b183
eleonaldosilva <eleonaldo.apt@gmail.com>
2020-08-15 13:53:29 -0300

    update

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const server = express()
 // configurando arquivos estaticos (css, scripts, imagens)
 server.use(express.static("public"))
 
+// configurando o nunjucks
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("view",{
+  express:server
+})
+
 // criei uma rota / e capturo o pedido do cliente  para responder
 server.get("/", function(req, res){
-  return res.sendFile(__dirname + "/index.html")
+  return res.render("index.html")
 })
 
 server.get("/idea", function(req, res){
-  return res.sendFile(__dirname + "/idea.html")
+  return res.render( "idea.html")
 })
 
 // usando a porta 3000 para o meu servidor

9cf90a6838b12bfb5c0679e4cf9be16402f09e92
eleonaldosilva <eleonaldo.apt@gmail.com>
2020-08-15 12:04:23 -0300

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log(__dirname + "/index.html")
+
+// // usei o express para criar e configurar o meu servidor 
+// const express = require("express")
+// const server = express()
+
+// // criei uma rota / e capturo o pedido do cliente  para responder
+// server.get("/", function(req, res){
+//   return res.sendFile(__dirname + "/index.html")
+// })
+
+// // usando a porta 3000 para o meu servidor
+// server.listen(3000)
